f21a3ec93e7d7082b1e14f0a3cc6c31982c08c15
Alex Teodorescu <56304311+alexsteo@users.noreply.github.com>
2021-12-03 14:46:29 +0200

    day3 part 2

--- days/day3.js
+++ days/day3.js
@@ -13,7 +13,7 @@ let bits = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 for (let d of data) {
     dataBits = d.split("");
     for (let i = 0; i < dataBits.length; i++) {
-        if (dataBits[i] == '1') {
+        if (dataBits[i] === '1') {
             bits[i]++;
         }
     }
@@ -34,3 +34,54 @@ console.log(gamma, eps, gamma * eps);
 console.log("--------------------------------------------------------------------------------------------------------")
 //--------------------------------------------------------------------------------------\\
 console.log("Part 2: ")
+
+const filterByDigit = (data, i, most) => {
+    let ones = 0;
+    let goodDigit;
+    let inputLength = data.length;
+
+    for (let d of data) {
+        if (d[i] === '1') {
+            ones++;
+        }
+    }
+
+    if (most) {
+        if (ones >= inputLength / 2) {
+            goodDigit = '1';
+        } else {
+            goodDigit = '0';
+        }
+    } else {
+        if (ones >= inputLength / 2) {
+            goodDigit = '0';
+        } else {
+            goodDigit = '1';
+        }
+    }
+
+    return data.filter(d => d[i] === goodDigit);
+}
+
+let oxygenData = data;
+
+for (let i = 0; i < data[0].length; i++) {
+    oxygenData = filterByDigit(oxygenData, i, true);
+    if (oxygenData.length === 1) break;
+}
+
+let co2Data = data;
+
+for (let i = 0; i < data[0].length; i++) {
+    co2Data = filterByDigit(co2Data, i, false);
+    if (co2Data.length === 1) break;
+}
+
+const co2Val = parseInt(co2Data[0], 2);
+const oxygenVal = parseInt(oxygenData[0], 2);
+
+console.log(oxygenVal, co2Val, oxygenVal * co2Val);
+
+
+
+
